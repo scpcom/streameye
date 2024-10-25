63a294745a58f5331597beb138b2416280864377
scpcom <scpcom@gmx.de>
2024-10-25 19:16:42 +0200

    kvm_stream: reduce JPEG_BUF_LEN

--- streameye.h
+++ streameye.h
@@ -31,7 +31,7 @@
 
 #define REQ_BUF_LEN             4096
 #define INPUT_BUF_LEN           1024 * 1024 /* 1MB */
-#define JPEG_BUF_LEN            1024 * 1024 * 10 /* 10MB */
+#define JPEG_BUF_LEN            1024 * 1024 * 3 /* 3MB */
 #define JPEG_START              "\xFF\xD8"
 #define JPEG_END                "\xFF\xD9"
 
